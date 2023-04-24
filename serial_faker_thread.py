7b8971556b9470882341122362797637914e2536
pluumenbrownie <git.wessel@beumertjes.nl>
2023-04-24 12:01:13 +0200

    Created serial_faker_thread.py

---
+++ serial_faker_thread.py
@@ -0,0 +1,54 @@
+import serial
+import time
+import re
+from typing import Generator
+from classes import *
+
+
+def faker() -> Generator:
+    while True:
+        time.sleep(1)
+        with open("fake_serial.txt", "rb") as file:
+            lines = file.readlines()
+            for line in lines:
+                time.sleep(0.01)
+                yield line
+
+
+heating_zone: list[HeatingZone] = []
+heating_zone.append(HeatingZone("Ground floor", 3))
+heating_zone.append(HeatingZone("Bathroom", 2))
+heating_zone.append(HeatingZone("Bedroom 1", 1))
+heating_zone.append(HeatingZone("Bedroom 2", 1))
+heating_zone.append(HeatingZone("Bedroom 3", 1))
+heating_zone.append(HeatingZone("Top floor", 1))
+
+# # with faker() as arduino:
+# while True:
+#     read_data = arduino.readline()
+for read_data in faker():
+    if read_data:
+        print(read_data)
+
+    if len(read_data) < 8:
+        continue
+    elif read_data[0:3] == b"[Th":
+        split_per_thermostat = read_data.split(b":")
+        for zone, status in zip(heating_zone, split_per_thermostat[1:7]):
+            zone.requesting = True if status[0:1] == b"1" else False
+    elif read_data[2:3] == b"]":
+        zone_nr: int = int(read_data[1:2])
+        numbers = re.findall(br"\d+", read_data[3:])
+        heating_zone[zone_nr].time_heating = int(numbers[0])
+        heating_zone[zone_nr].delay = int(numbers[1])
+        if (
+            heating_zone[zone_nr].time_heating == 0
+            and heating_zone[zone_nr].delay == 600
+        ):
+            heating_zone[zone_nr].heating = False
+        else:
+            heating_zone[zone_nr].heating = True
+    if read_data[0:3] == b"[5]":
+        for zone in heating_zone:
+            print(zone)
+        print(" ")
