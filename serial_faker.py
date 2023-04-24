faafd094395b0d27a74d16b8639633777c69ef6f
pluumenbrownie <git.wessel@beumertjes.nl>
2023-04-24 12:00:09 +0200

    Extracted HeatingZone to classes.py

--- serial_faker.py
+++ serial_faker.py
@@ -2,6 +2,7 @@ import serial
 import time
 import re
 from typing import Generator
+from classes import *
 
 
 def faker() -> Generator:
@@ -12,28 +13,6 @@ def faker() -> Generator:
             for line in lines:
                 time.sleep(0.01)
                 yield line
-
-
-class HeatingZone:
-    def __init__(self, name: str, priority: int) -> None:
-        self.name: str = name
-        self.priority: int = priority
-        self.requesting: bool = False
-        self.heating: bool = False
-        self.time_heating: int = 0
-        self.delay: int = 600
-
-    def __str__(self) -> str:
-        if self.heating:
-            if self.requesting:
-                return f"{self.name:>12s} || {'    R' if self.requesting else 'Not r'}equesting | Heating on | On for {self.time_heating:>3d} s"
-            else:
-                return f"{self.name:>12s} || {'    R' if self.requesting else 'Not r'}equesting | Heating on | On for {self.time_heating:>3d} s | Delay left: {self.delay} s"
-        else:
-            return f"{self.name:>12s} || {'    R' if self.requesting else 'Not r'}equesting | Heating off"
-    
-    def __repr__(self) -> str:
-        return f"{self.name}"
 
 
 heating_zone: list[HeatingZone] = []
