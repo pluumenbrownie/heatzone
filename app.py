677e94561ea449649cf2c9c65a9c1b913fd6ff7d
pluumenbrownie <git.wessel@beumertjes.nl>
2023-05-12 14:22:05 +0200

    Removed redundant imports

--- app.py
+++ app.py
@@ -4,17 +4,12 @@ import json
 import time
 import getpass
 
+import sqlalchemy as sql
 from flask import Flask, flash, session, redirect, render_template, request
 from flask_session import Session
-import sqlalchemy as sql
-from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy.orm import scoped_session, sessionmaker, aliased
-import requests
 from markupsafe import Markup
 
 from classes import *
-# from serial_thread import get_serial_data
-from serial_faker_thread import get_serial_data
 
 
 COLUMN_NAMES = ("timecode",
